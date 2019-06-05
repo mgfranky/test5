a3bdfa1d865d8f83621f97c008c56d7ac33486aa
mgfranky <40672380+mgfranky@users.noreply.github.com>
2019-06-05 16:06:48 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	title := "Jenkins X golang http devops test example"
+	if value := os.Getenv("title"); value != "" {
+		title = value
+	}
 
 	from := ""
 	if r.URL != nil {
